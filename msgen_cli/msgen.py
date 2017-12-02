7bed370b5db9d1c13ec8398c60ed2290c2f06867
Bob Davidson <bobd@microsoft.com>
2017-12-02 03:32:52 -0800

    update with PR feedbak

--- msgen_cli/msgen.py
+++ msgen_cli/msgen.py
@@ -21,6 +21,7 @@ VERSION = '0.7.2'
 def warn_for_package_update():
     """Check for updated version of msgen and warn if a newer version is available"""
 
+    pypiRoot = "https://pypi.python.org"
     connect_timeout = 0.1
     read_timeout = 0.1
 
@@ -30,20 +31,19 @@ def warn_for_package_update():
 
         # Construct the URL to get the msgen package information from pypi
         #   and extract pypi's version information from the payload
-        url = "https://pypi.python.org/pypi/{0}/json".format(malibuargs.PACKAGE_NAME)
+        url = "{0}/pypi/{1}/json".format(pypiRoot, malibuargs.PACKAGE_NAME)
         versions = requests.get(url, timeout=(connect_timeout, read_timeout)).json()["releases"].keys()
         versions.sort(key=StrictVersion, reverse=True)
         if VERSION < versions[0]:
             print("\n*** INFO ***"
-                  "\nA newer version of msgen is available."
-                  "  Please consider upgrading to v{0:s}."
+                  "\nA newer version of msgen is available.  Please consider upgrading to v{0:s}."
                   "\n  To upgrade, run: pip install --upgrade msgen"
                   "\n".format( versions[0] ))
 
     except (requests.Timeout, requests.ConnectionError):
         print("\n*** INFO ***"
-              "\nUnable to connect to https://pypi.python.org to check for updates."
-              "\n")
+              "\nUnable to connect to {0} to check for updates."
+              "\n".format(pypiRoot))
 
     return
 
